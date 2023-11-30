e8e1ffbfaaa117323460fae117f71867c775ee02
Ananias <amychelleoropezaortiz@gmail.com>
2023-11-30 16:46:29 -0500

    Finalizado(por ahora

--- JS/finVacaciones.js
+++ JS/finVacaciones.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
     const urlParams = new URLSearchParams(window.location.search);
     const restante3 = urlParams.get("restante3");
     const presupuesto3 = urlParams.get("presupuesto3");
+    const restante4 = urlParams.get("restante4");
+    const presupuesto4 = urlParams.get("presupuesto4");
     const contadorDias = urlParams.get("contadorDias");
     const contadorDias2 = urlParams.get("contadorDias2");
     const contadorDias3 = urlParams.get("contadorDias3");
@@ -16,6 +18,12 @@ document.addEventListener("DOMContentLoaded", function(){
             } else if (presupuesto3 !== null) {
                 let cuento = "Hildebrando a sobrevivido " + contadorDias + " dias, además, le sobró " + presupuesto3;
                 resultados.textContent = cuento;
+            } else if (restante4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias + " dias, además, le sobró " + restante4;
+                resultados.textContent = cuento;
+            } else if (presupuesto4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias + " dias, además, le sobró " + presupuesto4;
+                resultados.textContent = cuento;
             }
         } else if ( contadorDias2 !== null ) {
             if (restante3 !== null) {
@@ -23,6 +31,12 @@ document.addEventListener("DOMContentLoaded", function(){
                 resultados.textContent = cuento;
             } else if (presupuesto3 !== null) {
                 let cuento = "Hildebrando a sobrevivido " + contadorDias2 + " dias, además, le sobró " + presupuesto3;
+                resultados.textContent = cuento;
+            } else if (restante4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias2 + " dias, además, le sobró " + restante4;
+                resultados.textContent = cuento;
+            } else if (presupuesto4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias2 + " dias, además, le sobró " + presupuesto4;
                 resultados.textContent = cuento;
             }
         } else if ( contadorDias3 !== null ) {
@@ -32,6 +46,12 @@ document.addEventListener("DOMContentLoaded", function(){
             } else if (presupuesto3 !== null) {
                 let cuento = "Hildebrando a sobrevivido " + contadorDias3 + " dias, además, le sobró " + presupuesto3;
                 resultados.textContent = cuento;
+            } else if (restante4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias3 + " dias, además, le sobró " + restante4;
+                resultados.textContent = cuento;
+            } else if (presupuesto4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias3 + " dias, además, le sobró " + presupuesto4;
+                resultados.textContent = cuento;
             }
         } else if ( contadorDias4 !== null ) {
             if (restante3 !== null) {
@@ -39,6 +59,12 @@ document.addEventListener("DOMContentLoaded", function(){
                 resultados.textContent = cuento;
             } else if (presupuesto3 !== null) {
                 let cuento = "Hildebrando a sobrevivido " + contadorDias4 + " dias, además, le sobró " + presupuesto3;
+                resultados.textContent = cuento;
+            } else if (restante4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias4 + " dias, además, le sobró " + restante4;
+                resultados.textContent = cuento;
+            } else if (presupuesto4 !== null) {
+                let cuento = "Hildebrando a sobrevivido " + contadorDias4 + " dias, además, le sobró " + presupuesto4;
                 resultados.textContent = cuento;
             }
         }

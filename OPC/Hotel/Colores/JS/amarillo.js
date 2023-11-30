7af42a7fd9d26e963f25e533c68324c05f85cfec
Ananias <amychelleoropezaortiz@gmail.com>
2023-11-30 12:21:48 -0500

    Reestructurando la parte del hotel

--- OPC/Hotel/Colores/JS/amarillo.js
+++ OPC/Hotel/Colores/JS/amarillo.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function(){
     const restante3 = urlParams.get("restante3");
     const presupuesto3 = urlParams.get("presupuesto3");
     const contadorDias = urlParams.get("contadorDias");
+    const contadorDias2 = urlParams.get("contadorDias2");
+    const contadorDias3 = urlParams.get("contadorDias3");
+    const contadorDias4 = urlParams.get("contadorDias4");
+    const contadorDias5 = urlParams.get("contadorDias5");
 
     let meterse = document.getElementById("meterse");
     let solecito = document.getElementById("solecito");
@@ -18,10 +22,36 @@ document.addEventListener("DOMContentLoaded", function(){
         botonMuerte.setAttribute("id", "muerte");
 
         botonMuerte.addEventListener("click", function(){
-            if (restante3 !== null) {
-                location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias=" + contadorDias;
-            } else if (presupuesto3 !== null) {
-                location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias=" + contadorDias;
+            if ( contadorDias !== null ) {
+                if (restante3 !== null) {
+                    location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias=" + contadorDias;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias=" + contadorDias;
+                }
+            } else if ( contadorDias2 !== null ) {
+                if (restante3 !== null) {
+                    location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias2=" + contadorDias2;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias2=" + contadorDias2;
+                }
+            } else if ( contadorDias3 !== null ) {
+                if (restante3 !== null) {
+                    location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias3=" + contadorDias3;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias3=" + contadorDias3;
+                }
+            } else if ( contadorDias4 !== null ) {
+                if (restante3 !== null) {
+                    location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias4=" + contadorDias4;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias4=" + contadorDias4;
+                }
+            } else if ( contadorDias5 !== null ) {
+                if (restante3 !== null) {
+                    location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias5=" + contadorDias5;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias5=" + contadorDias5;
+                }
             }
         })
     })
@@ -29,5 +59,44 @@ document.addEventListener("DOMContentLoaded", function(){
     solecito.addEventListener("click", function() {
         let aviso = "Has decidido tomar el sol y pasarla bien, Hildebrando ha sobrevivido otro día :D";
         resultado.textContent = aviso;
+
+        let botonVida = document.createElement("button");
+        botonVida.innerHTML = "Continuar";
+        document.body.appendChild(botonVida);
+        botonVida.setAttribute("id", "vida");
+
+        botonVida.addEventListener("click", function(){
+            if ( contadorDias !== null ) {
+                let contadorDiasINT = parseInt(contadorDias);
+                let contadorDias2 = contadorDiasINT + 1;
+                if(restante3 !== null) {
+                    location.href = "../hotel2.html?restante3=" + restante3 + "&contadorDias2=" + contadorDias2;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../hotel2.html?presupuesto3=" + presupuesto3 + "&contadorDias2=" + contadorDias2;
+                }
+            } else if ( contadorDias2 !== null ) {
+                let contadorDias2INT = parseInt(contadorDias2);
+                let contadorDias3 = contadorDias2INT + 1;
+                if(restante3 !== null) {
+                    location.href = "../hotel3.html?restante3=" + restante3 + "&contadorDias3=" + contadorDias3;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../hotel3.html?presupuesto3=" + presupuesto3 + "&contadorDias3=" + contadorDias3;
+                }
+            } else if ( contadorDias3 !== null ) {
+                let contadorDias3INT = parseInt(contadorDias3);
+                let contadorDias4 = contadorDias3INT + 1;
+                if(restante3 !== null) {
+                    location.href = "../hotel4.html?restante3=" + restante3 + "&contadorDias4=" + contadorDias4;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../hotel4.html?presupuesto3=" + presupuesto3 + "&contadorDias4=" + contadorDias4;
+                }
+            } else if ( contadorDias4 !== null ) {
+                if (restante3 !== null) {
+                    location.href = "../../../finVacaciones.html?restante3=" + restante3 + "&contadorDias4=" + contadorDias4;
+                } else if (presupuesto3 !== null) {
+                    location.href = "../../../finVacaciones.html?presupuesto3=" + presupuesto3 + "&contadorDias4=" + contadorDias4;
+                }
+            }
+        })
     })
 })
--- OPC/Hotel/JS/hotel3.js
+++ OPC/Hotel/JS/hotel3.js
@@ -2,21 +2,20 @@ document.addEventListener("DOMContentLoaded", function(){
     const urlParams = new URLSearchParams(window.location.search);
     const restante3 = urlParams.get("restante3");
     const presupuesto3 = urlParams.get("presupuesto3");
-    let contadorDias = urlParams.get("contadorDias");
+    let contadorDias3 = urlParams.get("contadorDias3");
 
     let amarillo = document.getElementById("amarillo");
     let azul = document.getElementById("azul");
     let rojo = document.getElementById("rojo");
     let verde = document.getElementById("verde");
-    let cuentaDias = 1;
 
     amarillo.addEventListener("click", function() {
         if (restante3 !== null) {
-            let contadorDias = cuentaDias - 0;
-            location.href = "Colores/amarillo.html?restante3=" + restante3 + "&contadorDias=" + contadorDias;
+            let contadorDias4 = contadorDias3 - 0;
+            location.href = "Colores/amarillo.html?restante3=" + restante3 + "&contadorDias=" + contadorDias4;
         } else if (presupuesto3 !== null) {
-            let contadorDias = cuentaDias - 0;
-            location.href = "Colores/amarillo.html?presupuesto3=" + presupuesto3 + "&contadorDias=" + contadorDias;
+            let contadorDias4 = contadorDias3 - 0;
+            location.href = "Colores/amarillo.html?presupuesto3=" + presupuesto3 + "&contadorDias=" + contadorDias4;
         }
     })
     
